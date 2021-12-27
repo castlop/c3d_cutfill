476eb0b28074f0bd412d9d362d4ade1a4c178dd4
castlop <castlop.ma@gmail.com>
2021-12-27 00:42:20 -0600

    Feat: Group rows from table according to node type
    After getting tables from HTML text document, separate their rows as
    header rows and data rows.
    
    modified:   src/js/utilities.js

--- src/js/utilities.js
+++ src/js/utilities.js
@@ -15,3 +15,17 @@ export const getTablesFromText = (documentText) => {
   let doc = (new DOMParser()).parseFromString(documentText, 'text/html');
   return Array.from(doc.querySelectorAll('table'));
 }
+
+const groupTableRowsByNodeType = $tableRows => {
+  const groupRows = {headRows: [], dataRows: []};
+  $tableRows.forEach($tableRow => {
+    if ($tableRow < 1) return;
+    let $headerCells = Array
+                        .from($tableRow.cells)
+                        .filter($cell => $cell.nodeName === 'TH');
+    ($headerCells.length > 0 && $headerCells.length === $tableRow.cells.length)
+      ? groupRows.headRows.push($tableRow)
+      : groupRows.dataRows.push($tableRow);
+  });
+  return groupRows;
+}
