69177aff05d631859ec893aa75cae038a5240328
castlop <castlop.ma@gmail.com>
2022-01-08 02:25:50 -0600

    Feat: Add fetched data from tables to XLS workbook
    Save collected data in a given workbook and for the given sheetname,
    according to the type of the collected data, it means, if its an array
    of arrays or an array of objects. To process this data use Sheet JS
    library. Also, if worksheet already exists, append given data to the
    below end address cell of the contents on it.
    
    modified:   src/js/xls-output.js

--- src/js/xls-output.js
+++ src/js/xls-output.js
@@ -1,3 +1,19 @@
+const addDataToworksheet = (sheetName, workbook, dataCollection) => {
+  let belowAddress = '',
+      worksheet = workbook.Sheets[sheetName],
+      dataRowIsArray = dataCollection.every(dataRow => Array.isArray(dataRow));
+  if (worksheet === undefined) {
+    workbook.Sheets[sheetName] = (dataRowIsArray)
+      ? XLSX.utils.aoa_to_sheet(dataCollection)
+      : XLSX.utils.json_to_sheet(dataCollection);
+  } else {
+    belowAddress = getBelowAddress(worksheet['!ref']);
+    (dataRowIsArray)
+      ? XLSX.utils.sheet_add_aoa(worksheet, dataCollection, {origin: belowAddress})
+      : XLSX.utils.sheet_add_json(worksheet, dataCollection, {origin: belowAddress});
+  }
+}
+
 const getBelowAddress = cellAddress => {
   const rowPattern = /[0-9]$/,
         colPattern = /^[A-Z]/;
